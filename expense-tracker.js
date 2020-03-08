af778700f869b8ea7316724626e6020c89381e7e
M.K <miika.kallasoja@gmail.com>
2020-03-08 15:42:47 +0200

    Ability to group expenses by recipient

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,14 +1,16 @@
 
 
+let dataArr;
+
 let processDataButton = document.getElementById('process-data')
     processDataButton.addEventListener('click', function () {
             let sumkey = document.getElementById('headers-map-sum').value
             let datekey = document.getElementById('headers-map-date').value
             let recipientkey = document.getElementById('headers-map-recipient').value
             let dateformat = document.getElementById('date-format').value;
-            let dataArr = processData(sumkey, datekey, recipientkey, dateformat);
-            document.getElementById('income').innerText = "+" + dataArr[0];
-            document.getElementById('expenses').innerText = dataArr[1];
+            dataArr = processData(sumkey, datekey, recipientkey, dateformat);
+            document.getElementById('income').innerText = "+" + dataArr[0][1];
+            document.getElementById('expenses').innerText = dataArr[1][1];
             document.getElementById('difference').innerText = dataArr[2];
            
     });
@@ -31,21 +33,51 @@ let processDataButton = document.getElementById('process-data')
         document.getElementById('difference').innerText = "";
     }
 
- 
+
  let uploadCSVFile = document.getElementById('csv-file')
  uploadCSVFile.addEventListener('change', function (e) {
-     readFile(e.target.files[0])
+     readFile(e.target.files[0]) 
      moveProgress()
      setTimeout(function () {
          headerMapper(data, 'headers-map-sum')
          headerMapper(data, 'headers-map-date')
          headerMapper(data, 'headers-map-recipient')
-        // if (processDataButton.classList.value != 'hide')
          document.getElementById('field-maps').classList.remove('hide')
      }, 3000)
 
  });
 
+
+incomeButton = document.getElementById('income')
+expensesButton = document.getElementById('expenses')
+
+incomeButton.addEventListener('click', function () {
+    let sumkey = document.getElementById('headers-map-sum').value
+    let datekey = document.getElementById('headers-map-date').value
+    let recipientkey = document.getElementById('headers-map-recipient').value
+    renderItems(dataArr[0][0], sumkey, datekey, recipientkey)
+});
+
+
+
+expensesButton.addEventListener('click', function () {
+     let sumkey = document.getElementById('headers-map-sum').value
+     let datekey = document.getElementById('headers-map-date').value
+     let recipientkey = document.getElementById('headers-map-recipient').value
+     renderItems(dataArr[1][0], sumkey, datekey, recipientkey)
+    
+    });
+
+
+
+ let groupRecipientButton = document.getElementById('group-recipient-button');
+ groupRecipientButton.addEventListener('click', function (e) {
+    let sumkey = document.getElementById('headers-map-sum').value
+    let grouped = groupTransactionsByRecipient(dataArr[1][0], sumkey);
+    renderGroupedItemsTotal(grouped)
+
+
+ });
 
 
 function moveProgress() {
@@ -138,7 +170,6 @@ regexArr.forEach(function (regex) {
 function processData(sumkey, datekey, recipientkey, dateformat) {
      clearValues()
     data = getData()
-    console.log(data)
      let converted = convertToNumbers(data, sumkey)
      if (typeof converted[0][datekey] != 'object') {
      converted = convertDates(converted, datekey, dateformat)
@@ -148,7 +179,8 @@ function processData(sumkey, datekey, recipientkey, dateformat) {
      let expenses = getExpenses(sorted, datekey, sumkey, recipientkey)
      let income = getIncome(sorted, datekey, sumkey, recipientkey)
      let difference = getDifference(expenses, income)
-     return [income, expenses, difference]
+    renderItems(income[0], sumkey, datekey, recipientkey)
+     return [income, expenses, difference] // income and expenses are arrays
 }
 
 
@@ -219,32 +251,108 @@ function sortByDate(bankStatement, datekey) {
 
 function getExpenses(bankStatement, datekey, sumkey, recipientkey) {
     let total = 0;
+    let expenses = []
     for (let i = 0; i < bankStatement.length; i++) {
         if (bankStatement[i][sumkey] < 0) {
-        console.log(`Maksupäivä: ${bankStatement[i][datekey]} - Summa ${bankStatement[i][sumkey]} - Saaja ${bankStatement[i][recipientkey]}`)
+        // create new expenses array
+        expenses.push(bankStatement[i])        
+        // sum up total
         total += bankStatement[i][sumkey];
         }
     }
-    console.log(`Menot yhteensä: ${Math.round(total)}`)
-    return Math.round(total)
+    return [expenses, Math.round(total)]
 }
 
 function getIncome(bankStatement, datekey, sumkey, recipientkey) {
    let total = 0;
+   let income = []
    for (var i = 0; i < bankStatement.length; i++) {
         if (bankStatement[i][sumkey] > 0) {
-           console.log(`Maksupäivä: ${bankStatement[i][datekey]} - Summa ${bankStatement[i][sumkey]} - Saaja ${bankStatement[i][recipientkey]}`)    
-           document.getElementById('income').innerHTML = `<li>Maksupäivä: ${bankStatement[i][datekey]} - Summa ${bankStatement[i][sumkey]}</li>`
+            // create new income array
+            income.push(bankStatement[i])
+           // sum up total
            total += bankStatement[i][sumkey];
        }
    }
 
-   console.log(`Tulot yhteensä: ${Math.round(total)}`) 
-   return Math.round(total)
+   return [income, Math.round(total)]
    }
 
-function getDifference(expenses, income) {
-    let difference = income + expenses;
+
+
+function getDifference(expensesArr, incomeArr) {
+    let difference = incomeArr[1] + expensesArr[1];
     return Math.round(difference)
 }
 
+
+function renderItems(bankStatement, sumkey, datekey, recipientkey) {
+    document.getElementById('list').innerHTML = "";
+    for (let i = 0; i < bankStatement.length; i++) {
+    let li = document.createElement('li')
+    li.innerHTML = `${bankStatement[i][datekey].toDateString()} | ${bankStatement[i][sumkey]} | ${bankStatement[i][recipientkey]}`
+    document.getElementById('list').appendChild(li)
+    }
+}
+
+function groupTransactionsByRecipient(data, sumkey) {
+    recipients = []
+    let regex = /\b[a-zA-Z]*\s[a-zA-Z]*|\b[a-zA-Z]*/
+    // Parse and clean up recipient names for grouping
+    data.forEach(function (item) {
+        let match = regex.exec(item['Saajan nimi'].toLowerCase())[0].trim();
+        item['Saajan nimi'] = match;
+        if (recipients.indexOf(match) < 0)
+            recipients.push(match)
+    });
+    // Create an array where we store the grouped transactions
+    let groupedTransactions = []
+    recipients.forEach(function (rec) {
+        let obj = {};
+        obj['Recipient'] = rec;
+        obj['Transactions'] = []
+        groupedTransactions.push(obj)
+    });
+
+    // Do the actual sorting
+    for (let i = 0; i < data.length; i++) {
+        for (let y = 0; y < groupedTransactions.length; y++) {
+            if (data[i]['Saajan nimi'] == groupedTransactions[y]['Recipient'])
+                groupedTransactions[y]['Transactions'].push(data[i])
+        }
+    }
+
+    // sum up totals
+    groupedTransactions.forEach(function (item) {
+        let total = 0;
+        let transactions = item['Transactions'];
+        for (let i = 0; i < transactions.length; i++) {
+            total = total + transactions[i][sumkey]
+        }
+      item['Total'] = total;
+    });
+
+    // sort
+
+      groupedTransactions.sort(function (a, b) {
+          if (a['Total'] < b['Total']) {
+              return -1
+          } else if (b['Total'] > a['Total']) {
+              return 1
+          } else {
+              return 0
+          }
+      });
+     
+    return groupedTransactions
+
+}
+
+function renderGroupedItemsTotal(groupeditems) {
+    document.getElementById('list').innerHTML = ""
+    groupeditems.forEach(function (item) {
+        let li = document.createElement('li');
+     li.innerHTML = `${item['Recipient'].toUpperCase()}: ${Math.floor(item['Total'])}`
+     document.getElementById('list').appendChild(li);
+});
+};
